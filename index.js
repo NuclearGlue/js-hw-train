2b574184a51fb7286adcb910879a8d4107bac74e
Alexandr Sorokin <sanek1678@gmail.com>
2021-11-28 16:50:49 +0200

    28.11.2021

--- index.js
+++ index.js
@@ -1,129 +1,76 @@
-import { galleryItems } from "./app.js";
+import book from './books.js'
 
-const imagesList = document.querySelector('.gallery');
-const lightbox = document.querySelector('.lightbox');
-const lightboxImage = document.querySelector('.lightbox__image');
-const closeButton = document.querySelector('.lightbox__button');
-const overlayBox = document.querySelector('.lightbox__overlay');
+const root = document.querySelector('#root');
 
+const firstDiv = document.createElement('div');
+firstDiv.classList.add('div1');
+const secondDiv = document.createElement('div');
+secondDiv.classList.add('div2');
+const header = document.createElement('h1');
+const list = document.createElement('ul');
+list.classList.add('list');
+const text = document.createElement('p');
+const button = document.createElement('button');
 
-const addImages = galleryItems.map((picture, index) =>
-  `<li class="gallery__item"><a
-    class="gallery__link"
-    href="${picture.original}"
-  ><img  class="gallery__image" src='${picture.preview}' alt='${picture.description}' data-source = '${picture.original}' data-index = '${index}'> </a>
-  </li>`);
-imagesList.insertAdjacentHTML("beforeend", addImages.join(''));
+button.textContent = 'Add book';
+header.textContent = 'Our Books';
 
+root.append(firstDiv, secondDiv);
+firstDiv.append(header, list, button);
+const renderItem = document.querySelector('.div2');
+const listElem = document.querySelector('.list')
+localStorage.setItem('books', JSON.stringify(book));
 
-const eachImage = imagesList.querySelectorAll('.gallery__image');
-const sprayImage = [...eachImage];
-    
-    
-imagesList.addEventListener('click', bigPicture);
+function renderList() {
+  const bookItem = JSON.parse(localStorage.getItem("books"));
+  listElem.insertAdjacentHTML(
+    "afterbegin",
+    bookItem
+      .map(
+        (elem) => `<li id ="${elem.id}">
+          <p class = "description">${elem.title}</p>
+          <button class="edit">Edit</button>
+          <button class="delete">Delete</button>
+        </li>`
+      )
+      .join("")
+  );
+  const discription = document.querySelectorAll("p");
+  const btnEdit = document.querySelectorAll(".edit");
+  const btnDel = document.querySelectorAll(".delete");
 
-function bigPicture(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    const imageUrl = event.target.dataset.source;
-
-    lightbox.classList.add('is-open');
-    lightboxImage.src = imageUrl;
-    lightboxImage.alt = event.target.alt;
-     
-window.addEventListener('keydown', keyPress);
-}
-
-closeButton.addEventListener('click', modalClose);
-
-function modalClose(event) {
-    lightbox.classList.remove('is-open');
-
-    lightboxImage.src = '';
-    lightboxImage.alt = '';
-
-    window.removeEventListener('keydown', keyPress);
-}
-
-overlayBox.addEventListener('click', modalClose);
-
-function keyPress(event) {
-   
-    let currentIndex = 0;
-     galleryItems.forEach(image => {
-           if (image.original === lightboxImage.src) {
-               currentIndex = galleryItems.indexOf(image);
-            }
-     })
-    
-    
-    if (event.code === 'Escape') {
-        modalClose();
-    }
-    
-    if (event.code === 'ArrowRight') {
-
-        const nextIndex = currentIndex + 1;
-
-        lightboxImage.src = galleryItems[nextIndex].original;
-        // sprayImage.indexOf(event.target.)
-       
-        // const img = sprayImage.find(element => element.dataset.index == currentImage);
- 
-        // lightboxImage.src = img.dataset.source;
-        // lightboxImage.alt = img.alt;
-   
-        
-    }
-    if (event.code === 'ArrowLeft') {
-        const nextIndex = currentIndex - 1;
-
-        lightboxImage.src = galleryItems[nextIndex].original;
-    //     let imageIndex = event.target.firstElementChild.dataset.index;
-    // let nextIndex = Number(imageIndex) - 1;
-    //  const img = sprayImage.find(element => element.dataset.index == nextIndex);
-   
-    // console.log(img);
-
-    //   lightboxImage.src = img.dataset.source;
-    //   lightboxImage.alt = img.alt;
-        }
-         
-    
+  discription.forEach((elem) => elem.addEventListener("click", renderPrev));
+  btnEdit.forEach((elem) => elem.addEventListener("click", editBook));
+  btnDel.forEach((elem) => elem.addEventListener("click", deletBook));
 }
 
 
-    
+function renderPrev(event) {
 
+  const textDesc = event.currentTarget.textContent;
+const bookItem = JSON.parse(localStorage.getItem("books"));
 
+  const currentBook = bookItem.find(elem => elem.title === textDesc);
+  renderItem.innerHTML = `<h2>${currentBook.title}</h2>
+ <h3>${currentBook.author}</h3>
+  <img class = "book-image" src = ${currentBook.img}>
+ <p>${currentBook.plot}</p>`;
+  
+}
 
+function editBook() {
+  console.log("editBook");
+}
 
+function deletBook(event) {
+  const elemId = event.currentTarget.parentNode.id;
 
+  const bookItem = JSON.parse(localStorage.getItem("books"));
+  const newList = bookItem.filter((element) => element.id !== elemId)
+  localStorage.setItem('books', JSON.stringify(newList));
+  list.innerHTML = '';
+  secondDiv.innerHTML = '';
+  renderList();
+}
+renderList();
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li> */
